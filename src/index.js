c8894c946c618b8e7c00e76a2877b14fe0fdecc3
emersonvolkov@gmail.com <emersonvolkov@gmail.com>
2020-01-28 11:25:00 -0500

    Webpack y testing probados. Lunix, WSL, MacOS probados.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-#!/usr/bin/env node
 const yargs = require("yargs");
 
+const paths = require("./commands/paths")
+const resources = require("./commands/resources")
+const tags = require("./commands/tags")
+
+yargs.command(paths)
+yargs.command(resources)
+yargs.command(tags)
+
+
 yargs
-  .commandDir("commands")
-  .demandCommand()
   .help().argv;
 
 yargs.parse();
