91eb27a2abcdfedad66517b2fe882306abef3abf
emersonvolkov@gmail.com <emersonvolkov@gmail.com>
2020-02-10 20:41:34 -0500

    R2D41 realizando pruebas con reasonML. Al final he decidido seguir usando javascript y dejar fuera a reasonML y a TypeScript.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const paths = require("@commands/paths");
 const resources = require("@commands/resources");
 const tags = require("@commands/tags");
 
+yargs.version("0.0.10-alpha")
+
 yargs.command(paths);
 yargs.command(resources);
 yargs.command(tags);
